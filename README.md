fe8d6b3545c0e34c13e735313506f81da7b97509
AndrewChacon <andrew.b.chacon@gmail.com>
2023-09-26 22:39:56 -0400

    multi-threading

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# http-server
+# go-http-server
+
+https://www.digitalocean.com/community/tutorials/how-to-make-an-http-server-in-go
+
+Im using this article to learn how to set up a basic http server in golang
+Ive just recently developed an interest in the language to for building the backend server for future side projects/applications that I want to work on in the future.
--- main.go
+++ main.go
@@ -1,20 +1,69 @@
 package main
 
 import (
-	"errors"   // functions to manipulate errors
-	"fmt"      // formatted IO
-	"io"       // basic interface for IO
+	"context"
+	"errors" // functions to manipulate errors
+	"fmt"    // formatted IO
+	"io"     // basic interface for IO
+	"net"
 	"net/http" // http client and server implementations
 	"os"       // interface for operating system functionality
 )
 
 func main()  {
-	http.HandleFunc("/", getRoot)             // HANDLER FUNCTIONS
-	http.HandleFunc("/hello", getHello)       // HANDLER FUNCTIONS
 
-	err := http.ListenAndServe(":3333", nil)  // START SERVER, LISTEN FOR REQUESTS
+	mux := http.NewServeMux() // single connection between cilent and webserver
+	// Multi threading
 
-	if errors.Is(err, http.ErrServerClosed) {
+	mux.HandleFunc("/", getRoot)             // HANDLER FUNCTIONS
+	mux.HandleFunc("/hello", getHello)       // HANDLER FUNCTIONS
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+
+
+	serverOne := &http.Server {
+		Addr: ":3333",
+		Handler: mux,
+		BaseContext: func (l net.Listener) context.Context  {
+			ctx := context.WithValue(ctx, keyServerAdd, l.Addr().String())
+			return ctx
+		},
+	}
+
+	serverTwo := &http.Server {
+		Addr: ":4444",
+		Handler: mux,
+		BaseContext: func (l net.Listener) context.Context  {
+			ctx := context.WithValue(ctx, keyServerAdd, l.Addr().String())
+			return ctx
+		},
+	}
+
+	go func ()  {
+		err:= serverOne.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Server one is closed\n")
+		} else if err != nil {
+			fmt.Printf("Error listening for server one: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	go func ()  {
+		err:= serverTwo.ListenAndServe()
+		if errors.Is(err, http.ErrServerClosed) {
+			fmt.Printf("Server two is closed\n")
+		} else if err != nil {
+			fmt.Printf("Error listening for server two: %s\n", err)
+		}
+		cancelCtx()
+	}()
+
+	<-ctx.Done()
+
+	err := http.ListenAndServe(":3333", mux)  // START SERVER, LISTEN FOR REQUESTS
+
+	if errors.Is(err, http.ErrServerClosed) {  // checking if server is getting closed
 		fmt.Printf("Server Closed\n")
 	} else if err != nil { // if there is an error shut the shit down
 		fmt.Printf("error starting server %s\n", err)
@@ -23,12 +72,16 @@ func main()  {
 
 }
 
+const keyServerAdd = "serverAdd"
+
 func getRoot(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got / Request")
+	ctx := r.Context()
+	fmt.Printf("%s got /request\n", ctx.Value(keyServerAdd))
 	io.WriteString(w, "This is my website\n") // Writes to page
 }
 
 func getHello(w http.ResponseWriter, r *http.Request) {
-	fmt.Println("Got /hello request")
+	ctx := r.Context()
+	fmt.Printf("%s got: /hello request\n", ctx.Value(keyServerAdd))
 	io.WriteString(w, "Hello, HTTP!") // Writes to page
 }
